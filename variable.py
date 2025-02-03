5424454bc897970563f382ee23fbbf81e9bba40f
hudsonnbenhuraa@gmail.com <HudsonWayne>
2025-02-03 14:03:54 +0200

    Touched everything on Variables

---
+++ variable.py
@@ -0,0 +1,11 @@
+#Variable = A container for a value (int, float, bool, string)
+
+first_name = ("Wayne")
+food = ("Pizza")
+email = ("hudson@example.com")
+
+print(first_name)
+print(f"Your favorite food is {food}")
+print(f"Your email address is {email}")
+
+#Variable [first_name] has a value of Wayne, so when you print it returns wayne
