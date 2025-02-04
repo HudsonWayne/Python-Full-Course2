c57cc1ad74fd340176c1d31979de1020cb12071a
hudsonnbenhuraa@gmail.com <HudsonWayne>
2025-02-04 09:05:26 +0200

    finishing up variables

--- variable.py
+++ variable.py
@@ -9,3 +9,7 @@ print(f"Your favorite food is {food}")
 print(f"Your email address is {email}")
 
 #Variable [first_name] has a value of Wayne, so when you print it returns wayne
+
+int = 25
+
+print(f"Your age is {int} years old")
