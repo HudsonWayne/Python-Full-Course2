21051c0603e8cf66a68b20097dfa41b1cbd55c83
hudsonnbenhuraa@gmail.com <HudsonWayne>
2025-02-14 10:55:08 +0200

    format specifiers

---
+++ fomart.py
@@ -0,0 +1,10 @@
+# format specifiers = {value:flag}format values based on what flags are inserted
+
+
+price1 = 3.142
+price2= -95.3738
+price3 =12.88
+
+print(f"The price 1 is {price1:.2f}")
+print(f"The price 2 is {price2:.2f}")
+print(f"The price 3 is {price3:.2f}")
