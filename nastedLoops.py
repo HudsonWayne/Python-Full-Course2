07b337f10fba800d47960a8f4a86f79ab6069ff2
hudsonnbenhuraa@gmail.com <HudsonWayne>
2025-02-18 08:44:43 +0200

    nested loop, putting another loop inside another loop

---
+++ nastedLoops.py
@@ -0,0 +1,12 @@
+#nested loops = The inner loops will finish all of its iterations before finishing finishing one iteration of the outer loop
+
+rows = int(input("How many rows?:"))
+columns = int(input("How many columns?:"))
+symbol = input("Enter symbol:")
+
+
+for i in range(rows):
+    for j in range(columns):
+        print(symbol, end ="")
+    print()    
+        
