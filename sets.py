862b83d5bfce087732591c3fdbdbdce7bf554412
hudsonnbenhuraa@gmail.com <HudsonWayne>
2025-02-18 11:45:06 +0200

    sets

---
+++ sets.py
@@ -0,0 +1,14 @@
+# set = collection which is unordered, indexing.
+#           No  duplicates values
+
+
+utensils = {"fork", "spoon", "knife"}
+dishes = {"bowl","plate","cup"}
+
+# utensils.add("napkin")
+# utensils.remove("fork")
+# utensils.clear()
+
+for x in utensils:
+    print(x)
+    
