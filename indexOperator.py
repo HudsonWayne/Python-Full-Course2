05a007abda83be0c38656a06f64d33071faf4152
hudsonnbenhuraa@gmail.com <HudsonWayne>
2025-02-18 13:34:49 +0200

    indexOP

---
+++ indexOperator.py
@@ -0,0 +1,12 @@
+#index operator [] = gives access to a sequence's elements (str,list,tuples)
+
+name = "bro Code"
+
+# if(name[0].islower()):
+#     name = name.capitalize()
+
+first_name = name[:3].upper()
+last_name = name[4:].lower()
+    
+print(first_name)
+print(last_name)
