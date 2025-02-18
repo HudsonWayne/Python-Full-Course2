23b427582cd327d880e4e44a42923940c39ea7ea
hudsonnbenhuraa@gmail.com <HudsonWayne>
2025-02-18 09:17:46 +0200

    controlling loops

---
+++ loopControl.py
@@ -0,0 +1,25 @@
+# Loop Control Statements = change a loops execution from its normal sequence
+
+#break =    used to terminate the loop entirely
+#continue = skips to the next iteration of the loop
+#pass =  does nothing, acts as a placeholder
+
+# while True:
+#     name = input("Enter the name: ")
+#     if name != "":
+#         break
+
+
+# phone_number = "077-123-456"
+
+# for i in phone_number:
+#     if i =="-":
+#         continue
+#     print(i)
+
+for i in range(1, 21):
+
+    if i == 13:
+        pass
+    else:
+        print(i)
