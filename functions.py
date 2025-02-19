629589da7459d8ea2fb2ee37bd66c8dbe53baa91
hudsonnbenhuraa@gmail.com <HudsonWayne>
2025-02-19 11:56:05 +0200

    return statements

---
+++ functions.py
@@ -0,0 +1,8 @@
+#function = a block of code which is executed   
+#               when it is called
+
+
+
+def hello (first, second):
+    print("Hello"+first+" "+second)
+    print("Have a nice day")
---
+++ returnStament.py
@@ -0,0 +1,7 @@
+#return statement = Functions send python values    objects back to caller. These values/objects are known as the functions return value
+
+
+def multiply(number1, number2):
+    result = number1 * number2
+    return result
+print(multiply(6,8))
