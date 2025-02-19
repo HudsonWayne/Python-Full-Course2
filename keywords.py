b9b835893d83624a862ce2a397683e4a0b59b263
hudsonnbenhuraa@gmail.com <HudsonWayne>
2025-02-19 14:06:32 +0200

    keywords

---
+++ keywords.py
@@ -0,0 +1,9 @@
+#Keywords arguments = arguments preceded by 
+#       an identifier when we pass them to a further
+
+
+def hello(first, middle, last):
+    print("Hello " + first + " " + middle + " " + last)
+
+
+hello(last="Bro", middle="Dude", first="Code")
