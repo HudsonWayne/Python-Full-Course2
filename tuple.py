ad7dd1028aa9e2961a562a5ffe0b9ccbc613eec9
hudsonnbenhuraa@gmail.com <HudsonWayne>
2025-02-25 10:55:13 +0200

    sliced

--- tuple.py
+++ tuple.py
@@ -1,6 +1,11 @@
 #Tuple = collection which is ordered and unchangeable
             #used to group together related data
             
-my_tuple = (10, 20, 30, 40, 50)
+# my_tuple = (10, 20, 30, 40, 50)
 
-print(my_tuple[2])
+# print(my_tuple[2])
+
+
+numbers = (5, 10, 15, 20, 25)
+slice = numbers[2:5]
+print(slice)
