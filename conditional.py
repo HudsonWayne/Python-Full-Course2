957b2602f07ca55b8d2777f1520491685d1f7cd2
hudsonnbenhuraa@gmail.com <HudsonWayne>
2025-02-26 15:28:59 +0200

    even and odd numbers

--- conditional.py
+++ conditional.py
@@ -1,6 +1,6 @@
-set1 = {1, 2, 3, 4}
-set2 = {3, 4, 5, 6}
+num = int(input("Enter number: "))
 
-intersection = set1.intersection(set2)
-
-print(intersection)
+if num % 2 == 0:
+    print("Its is odd number")
+else:
+    print("Its is even number")    
