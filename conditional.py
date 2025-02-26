57dcb4e8e238ff8b28e0006c557a0bc73fbc014f
hudsonnbenhuraa@gmail.com <HudsonWayne>
2025-02-26 14:54:08 +0200

    sets

--- conditional.py
+++ conditional.py
@@ -1,2 +1,6 @@
-num = 5
-print("Positive" if num > 0 else "negative")
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+
+intersection = set1.intersection(set2)
+
+print(intersection)
--- tuple.py
+++ tuple.py
@@ -6,6 +6,18 @@
 # print(my_tuple[2])
 
 
-numbers = (5, 10, 15, 20, 25)
-slice = numbers[2:5]
-print(slice)
+# numbers = (5, 10, 15, 20, 25)
+# slice = numbers[2:5]
+# print(slice)
+
+# my_list = [1, 2, 3, 4, 5]
+# my_list[0] = 10
+# print(my_list)  
+
+fruits = ("orange","banana","apple")
+fruit_list = list(fruits)
+
+fruit_list.append("mango")
+
+fruits = tuple(fruit_list)
+print(fruits)
