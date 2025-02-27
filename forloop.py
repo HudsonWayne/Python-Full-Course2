9bfca328ca2ec488f71defd9b33934092a8e5bee
hudsonnbenhuraa@gmail.com <HudsonWayne>
2025-02-27 09:59:50 +0200

    loops

--- forloop.py
+++ forloop.py
@@ -1,8 +1,3 @@
-import time
-
-for seconds in range(10, 0, -1):
-    print(seconds)
-    time.sleep(1)
-    
-    if seconds == 0:
-        print("Welcome to mufakose!")
+fruits = ["apple", "orange", "banana"]
+for fruit in fruits:
+    print(fruit)
