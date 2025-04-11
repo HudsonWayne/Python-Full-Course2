2901f62f75b761fbbae580dabd63348d5fb878c8
HudsonWayne <hudsonnbenhuraa@gmail.com>
2025-04-11 11:39:41 +0200

    initial commit

--- projects/qrCode.py
+++ projects/qrCode.py
@@ -4,4 +4,4 @@ import qrcode
 img = qrcode.make("https://github.com/HudsonWayne?tab=overview&from=2025-04-01&to=2025-04-11")
 
 # Save the image
-img.save("hudson_qr.png")
+img.save("HudsonWayne_qr.png")
